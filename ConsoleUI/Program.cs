74b71179150196cf65d7a415f79465f49221562d
Derek Sparks <drsparks@gmail.com>
2020-04-23 22:13:27 -0700

    Initial Import

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayAuthor();
+            string firstName = GetUsersName("What is your first name?");
+            string lastName = GetUsersName("What is your last name?");
+            WelcomeUser(firstName, lastName);
+
+
+            Console.ReadLine();
+        }
+
+        private static void SayAuthor()
+        {
+            Console.WriteLine("************************************");
+            Console.WriteLine("Written by Derek Sparks");
+            Console.WriteLine("for the Foundation in C# course");
+            Console.WriteLine("************************************");
+        }
+
+        private static void WelcomeUser(string firstName, string lastName)
+        {
+            Console.WriteLine($"Hello { firstName } { lastName }");
+        }
+
+        private static string GetUsersName(string message)
+        {
+            Console.WriteLine(message);
+            string output = Console.ReadLine();
+
+            if (output.Length > 1)
+            {
+                return output;
+            }
+          
+            return "<Input not recognized. Try again.>";
+        }
+
+    }
+}
